b264f833ead422331c14a03f0ee1502c2ccc17e9
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-05-02 02:11:35 +0200

    first commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/Invoice', title: 'Invoice',  icon:'receipt', class: '' },
+    { path: '/Category', title: 'Category',  icon:'category', class: '' },
+    { path: '/Products', title: 'Product',  icon:'local_mall', class: '' },
+    { path: '/Customers', title: 'Customer',  icon:'groups', class: '' },
+    { path: '/Provider', title: 'Provider',  icon:'transfer_within_a_station', class: '' },
+
+    { path: '/Order-Sale', title: 'Sale-Order',  icon:'add_shopping_cart', class: '' },
+    { path: '/Purchase-Order', title: 'Purchase-Order',  icon:'shopping_cart', class: '' },
+    //{ path: '/monComp', title: 'monComp',  icon:'toc', class: '' }, 
+    //{ path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    //{ path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+    //{ path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    //{ path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: '/Payments', title: 'Payments',  icon:'payments', class: 'active-pro' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,85 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { TypographyComponent } from '../../typography/typography.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { MapsComponent } from '../../maps/maps.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { MonCompComponent } from 'app/mon-comp/mon-comp.component';
+import { InvoiceComponent } from 'app/invoice/invoice.component';
+import { CategoryComponent } from 'app/category/category.component';
+import { ProductsComponent } from 'app/products/products.component';
+import { CustomersComponent } from 'app/customers/customers.component';
+import { ProviderComponent } from 'app/provider/provider.component';
+import { OrderSaleComponent } from 'app/order-sale/order-sale.component';
+import { PurchaseOrderComponent } from 'app/purchase-order/purchase-order.component';
+import { PaymentsComponent } from 'app/payments/payments.component';
+
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //   path: '',
+    //   children: [ {
+    //     path: 'icons',
+    //     component: IconsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'maps',
+    //         component: MapsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'typography',
+    //         component: TypographyComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'upgrade',
+    //         component: UpgradeComponent
+    //     }]
+    // }
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'upgrade',        component: UpgradeComponent },
+    { path: 'monComp',        component: MonCompComponent },
+    { path: 'Invoice',        component: InvoiceComponent },
+    { path: 'Category',        component: CategoryComponent },
+    { path: 'Products',        component: ProductsComponent },
+    { path: 'Customers',        component: CustomersComponent },
+    { path: 'Provider',        component: ProviderComponent },
+    { path: 'Order-Sale',        component: OrderSaleComponent },
+    { path: 'Purchase-Order',        component: PurchaseOrderComponent },
+    { path: 'Payments',        component: PaymentsComponent },
+
+    
+
+
+];
