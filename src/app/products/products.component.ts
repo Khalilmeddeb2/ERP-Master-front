2d3eff371af48100e98d25bf051527d98cc3a686
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-05-03 05:31:52 +0200

    Update products.component.ts

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductEntity } from 'app/models/product-entity.model';
+import { ProductServiceService } from 'app/productService/product-service.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [ProductServiceService]
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private productServiceService : ProductServiceService) {}
+  products :ProductEntity [] ;
   ngOnInit(): void {
+    debugger ;
+    this.productServiceService.getProduct().subscribe(e=>{
+this.products=e
+console.log("========>",e) ;
+
+    })
+   
   }
 
 }
