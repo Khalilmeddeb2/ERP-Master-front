a2599981c8fda61d63750acde5e6011d33dc8166
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-05-07 02:12:20 +0200

    Update components.module.ts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MatPaginatorModule,
   ],
   declarations: [
     FooterComponent,
