cd1121bdb1ead16d82a4b1cae22f241ce422003b
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-05-28 00:06:08 +0200

    hhhh

---
+++ src/app/services/payments.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PaymentEntity } from 'app/models/payment-entity';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentsService {
+
+  private baseURL=  "http://localhost:9000/api/payments";
+
+  constructor(private http: HttpClient) { }
+
+
+  getPaymentsList() : Observable<PaymentEntity[]>{
+    return this.http.get<PaymentEntity[]>(`${this.baseURL}`);
+  }
+}
